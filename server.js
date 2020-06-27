8089b6c1ca2f07519651d26ccf39d35412d229ed
Meianna Oeser <meianna.oeser@gmail.com>
2020-06-26 23:41:26 -0700

    fixed code again

--- server.js
+++ server.js
@@ -4,7 +4,7 @@ const PORT = process.env.PORT || 8080;
 const app = express();
 app.use(express.urlencoded({ extended: true }));
 app.use(express.json());
-app.use(express.static("public"));
+app.use(express.static("client"));
 app.use(require("./routes/api-routes.js"));
 app.use(require("./routes/client-routes.js"));
 app.listen(PORT, function () {
