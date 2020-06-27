2a1e4ce1a696dab9f5653ee43b9ccc22722d529d
Meianna Oeser <meianna.oeser@gmail.com>
2020-06-26 23:23:36 -0700

    updated final code

--- server.js
+++ server.js
@@ -6,7 +6,7 @@ app.use(express.urlencoded({ extended: true }));
 app.use(express.json());
 app.use(express.static("public"));
 app.use(require("./routes/api-routes.js"));
-app.use(require("./routes/html-routes.js"));
+app.use(require("./routes/client-routes.js"));
 app.listen(PORT, function () {
   console.log(`server running listening on PORT ${PORT}`);
 });
